a2d96b2e01a20808316a00f8b7a119fc0eef02ad
j32u4ukh <j32u4ukh@gmail.com>
2022-03-05 18:04:15 +0800

    Lesson 41

--- Assets/_Scripts/Systems/ShootSystem.cs
+++ Assets/_Scripts/Systems/ShootSystem.cs
@@ -19,14 +19,25 @@ public class ShootSystem : JobComponentSystem
                 .WithStructuralChanges()
                 .ForEach((Entity entity, ref Translation position, ref Rotation rotation, ref ShipData ship_data) =>
                 {
-                    foreach(float3 bullet_spawn_point in bullet_spawn_points)
-                    {
-                        var instance = manager.Instantiate(ship_data.bullet_entity);
+                    float3 direction = GameDataManager.instance.wps[ship_data.current_wp] - position.Value;
 
-                        manager.SetComponentData(instance, new Translation { Value = position.Value + math.mul(rotation.Value, bullet_spawn_point) });
-                        manager.SetComponentData(instance, new Rotation { Value = rotation.Value });
-                        manager.SetComponentData(instance, new LifeTimeData { value = 1f });
-                    }
+                    // 利用 餘弦定理 計算兩向量夾角(angle)
+                    float cos = math.dot(math.forward(rotation.Value), direction);
+                    float len_a = math.length(math.forward(rotation.Value));
+                    float distance = math.length(direction);
+                    float angle = math.acos(cos / (len_a * distance));
+
+                    if((angle < math.radians(5)) && (distance < 100.0f))
+                    {
+                        foreach (float3 bullet_spawn_point in bullet_spawn_points)
+                        {
+                            var instance = manager.Instantiate(ship_data.bullet_entity);
+
+                            manager.SetComponentData(instance, new Translation { Value = position.Value + math.mul(rotation.Value, bullet_spawn_point) });
+                            manager.SetComponentData(instance, new Rotation { Value = rotation.Value });
+                            manager.SetComponentData(instance, new LifeTimeData { value = 1f });
+                        }
+                    }                    
                 })
                 .Run();
 
